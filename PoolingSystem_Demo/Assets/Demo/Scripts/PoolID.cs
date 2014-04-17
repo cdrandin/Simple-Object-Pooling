9f98925c466aa2853e08547a9fc5c28b0abff27e
cdrandin <cdrandin@hotmail.com>
2014-04-17 01:45:00 -0700

    Added Simple Object Pooling Demo

---
+++ PoolingSystem_Demo/Assets/Demo/Scripts/PoolID.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PoolID : MonoBehaviour {
+	[SerializeField]private int _id;
+	private bool _init;
+
+	public int id
+	{
+		get { return _id; }
+	}
+
+	void Awake()
+	{
+		_init = false;
+	}
+
+	public void GenerateID(GameObject obj)
+	{
+		if(_init) return; // called only once
+		_id = obj.transform.GetInstanceID();
+		_init = true;
+	}
+}
