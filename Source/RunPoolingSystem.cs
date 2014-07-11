65a405346b9549858fd2ad25bd4a6040dec89132
cdrandin <cdrandin@hotmail.com>
2014-07-10 23:45:13 -0700

    Update RunPoolingSystem.cs

--- Source/RunPoolingSystem.cs
+++ Source/RunPoolingSystem.cs
@@ -1,13 +1,15 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 
 public class RunPoolingSystem : MonoBehaviour 
 {
+	public bool test;
+
 	public List<GameObject> objects_to_pool; 
 
 	public List<GameObject> objects_in_the_pool;
-
+	
 	// Use this for initialization
 	void Start () 
 	{
@@ -18,20 +20,30 @@ public class RunPoolingSystem : MonoBehaviour
 	{
 		if(Input.GetKeyDown(KeyCode.Space))
 		{
-			GameObject obj = PoolingSystem.instance.PS_Instantiate(objects_to_pool[Random.Range(0, objects_to_pool.Count)], 
-			                                                       new Vector3(Random.Range(-5.0f, 5.0f), 3.0f, Random.Range(-5.0f, 5.0f)),
-			                                                       Quaternion.identity);
+			GameObject obj = null;
+			obj = PoolingSystem.instance.PS_Instantiate(objects_to_pool[Random.Range(0, objects_to_pool.Count)], 
+				                                        new Vector3(Random.Range(-5.0f, 5.0f), 3.0f, Random.Range(-5.0f, 5.0f)),
+				                                        Quaternion.identity);
+
 			if(obj == null) return;
 
 			objects_in_the_pool.Add(obj);
 		}
-
 		else if(Input.GetKeyDown(KeyCode.Backspace))
 		{
 			if(objects_in_the_pool.Count > 0)
 			{
 				int i = Random.Range(0, objects_in_the_pool.Count);
-				PoolingSystem.instance.PS_Destroy(objects_in_the_pool[i]);
+
+				if(!test)
+				{
+					PoolingSystem.instance.PS_Destroy(objects_in_the_pool[i]);
+				}
+				else
+				{
+					PoolingSystem.instance.PS_Destroy(objects_in_the_pool[i]);
+				}
+
 				objects_in_the_pool.RemoveAt(i);
 			}
 		}
